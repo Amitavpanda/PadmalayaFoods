198a74c31fb8580020fe8f03867150c85758207f
Amitav Panda <amitavpanda@Amitav-ka-MacBook-Pro.local>
2025-03-24 00:33:38 +0530

    add the backend logic for product, category, orders

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -1,12 +1,18 @@
 import { Express, Request, Response } from "express";
 import validate from "./middleware/validateResource.js";
-import {registerUserSchema, loginUserSchema, otpVerificationSchema, resendOtpSchema} from "@repo/validations/userSchema";
+import { registerUserSchema, loginUserSchema, otpVerificationSchema, resendOtpSchema } from "@repo/validations/userSchema";
 import { loginHandler, logOutHandler, otpVerificationHandler, registerHandler, resendOtpHandler } from "./controllers/users.controller.js";
 import { authenticateAccessToken } from "./middleware/authMiddleware.js";
+import { addCategorySchema, editCategorySchema, deleteCategorySchema } from "@repo/validations/categorySchema";
+import { addProductSchema, editProductSchema, deleteProductSchema } from "@repo/validations/productSchema";
+import { addCategoryHandler, deleteCategoryHandler, editCategoryHandler } from "./controllers/category.controller.js";
+import { addProductHandler, deleteProductHandler, editProductHandler } from "./controllers/product.controller.js";
+import { addOrderSchema, editOrderSchema, deleteOrderSchema } from "@repo/validations/orderSchema";
+import {addOrderHandler, editOrderHandler, getOrdersHandler, deleteOrderHandler} from "./controllers/order.controller.js";
 
-function routes(app : Express){
-    app.get('/healthcheck', (req : Request, res : Response)  => res.sendStatus(200));
-    
+function routes(app: Express) {
+    app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+
     app.use((req: Request, res: Response, next) => {
         console.log("CORS Headers:", res.getHeaders()); // Log headers for each route
         next();
@@ -16,24 +22,42 @@ function routes(app : Express){
     app.post('/otpVerification', validate(otpVerificationSchema), otpVerificationHandler)
     app.post('/login', validate(loginUserSchema), loginHandler);
     app.post('/logout', logOutHandler);
-    app.post('/resendOtp', validate(resendOtpSchema) , resendOtpHandler);
+    app.post('/resendOtp', validate(resendOtpSchema), resendOtpHandler);
     app.get('/profile', authenticateAccessToken, (req, res) => {
-        res.json({ message: 'User profile'});
+        res.json({ message: 'User profile' });
     });
+    app.post('/addCategory', authenticateAccessToken, validate(addCategorySchema), addCategoryHandler);
+
+    app.put('/editCategory/:categoryId', authenticateAccessToken, validate(editCategorySchema), editCategoryHandler);
+
+    app.delete('/deleteCategory/:categoryId', authenticateAccessToken, validate(deleteCategorySchema), deleteCategoryHandler);
+
+    app.post('/addProduct', authenticateAccessToken, validate(addProductSchema), addProductHandler);
+
+    app.put('/editProduct/:productId', authenticateAccessToken, validate(editProductSchema), editProductHandler);
+
+    app.delete('/deleteProduct/:productId', authenticateAccessToken, validate(deleteProductSchema), deleteProductHandler);
 
     // app.get('/paymentAPI', authenticateAccessToken, (req, res) => {
+    app.post('/addOrder', authenticateAccessToken, validate(addOrderSchema), addOrderHandler);
+
+    app.put('/editOrder/:orderId', authenticateAccessToken, validate(editOrderSchema), editOrderHandler);
+
+    app.delete('/deleteOrder/:orderId', authenticateAccessToken, validate(deleteOrderSchema), deleteOrderHandler);
+
+    app.get('/getOrders', authenticateAccessToken, getOrdersHandler);
     //     res.json({ message: 'User profile'});
 
     //     // 
     // });
 
-    
+
     // app.get('/addTOCart', (req, res) => {
     //     res.json({ message: 'User profile'});
 
-        // 
+    // 
     // });
-    
+
 
 
 
---
+++ apps/api/src/services/product.service.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from "@repo/db/client";
+import { error, info } from "@repo/logs/logs";
+import { AddProductSchema, EditProductSchema, DeleteProductSchema } from "@repo/validations/productSchema";
+
+
+const prisma = new PrismaClient();
+
+export async function addProductService(input: AddProductSchema) {
+    const { productName, categoryId, pricing, image, stockStatus } = input.body;
+    try {
+        const newProduct = await prisma.product.create({
+            data: {
+                productName,
+                categoryId,
+                pricing,
+                image,
+                stockStatus,
+            },
+        });
+        return {success : true, data : newProduct};
+    } catch (e : any) {
+        error("Error in addProductService", e);
+        return e;
+    }
+    
+}
+
+export async function editProductService(input: EditProductSchema) {
+    const { productId } = input.params;
+    const { productName, categoryId, pricing, image, stockStatus } = input.body;
+
+    try {
+        const product = await prisma.product.update({
+            where: {
+                id : productId,
+            },
+            data: {
+                productName,
+                categoryId,
+                pricing,
+                image,
+                stockStatus,
+            },
+        });
+        return {success : true, data : product};
+    } catch (e : any) {
+        error("Error in editProductService", e);
+        return e;
+    }
+}
+
+export async function deleteProductService(input: DeleteProductSchema) {
+    const { productId } = input.params;
+
+    try {
+        const product = await prisma.product.delete({
+            where: {
+                id : productId,
+            },
+        });
+        return product;
+    } catch (e : any) {
+        error("Error in deleteProductService", e);
+        return e;
+    }
+}
